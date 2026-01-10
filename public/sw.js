f523cb037c15c5f38439ae8b8c252bf6e7d6a7d8
Sanjay Ram J <j.sanjayramdata@gmail.com>
2026-01-10 15:07:52 +0530

    updation

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10442710
+    "zoneId": 10442737
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
